7362743cde98ad79db37322b1b16f88172a8bff0
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2009-10-02 04:43:13 +0000

    * script to create XPI * the current XPI for code in this svn revision

---
+++ addon-subtitles/xpi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+zip wikisubs-svn.xpi content/overlay.xul content/wikisubs.js content/wikisubs.xml skin/subtitlesButton.png skin/subtitlesButton.svg skin/wikisubs.css skin/wikisubsHookup.css install.rdf LICENSE.TXT chrome.manifest
+
