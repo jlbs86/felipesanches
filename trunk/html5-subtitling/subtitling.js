f5d795deed5fd2e4ba89466b5c48e4a860d79ddf
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2009-11-23 03:02:27 +0000

    step 2 implementation

--- trunk/html5-subtitling/subtitling.js
+++ trunk/html5-subtitling/subtitling.js
@@ -1,27 +1,25 @@
 //Globar vars:
 var mode = "playback";
 var current_subtitle = null;
+var current_title_sync;
 var subtitles_textbox = [];
 var autoskipback;
 var autoskipback_ammount;
 var autoskip_timeout;
+var video;
+var holdingkey=false;
+var subtitles_p;
 
 //---------------
-function sync_mode(){
-  var transcriptiondiv = document.getElementById("transcription");
-  var syncdiv = document.getElementById("syncing");
-
-  transcriptiondiv.style.display="none";
-  syncdiv.style.display="block";
-}
 
 function displaySubtitles_sync(){
   if (current_subtitle == null){
-    subtitles_textbox[0].value = "";
-    subtitles_textbox[1].value = "";
-    subtitles_textbox[2].value = "";
-    subtitles_textbox[3].value = "";
-    subtitles_textbox[4].value = "";
+    subtitles_textbox[0].innerHTML = "[silence]";
+    subtitles_textbox[1].innerHTML = "[silence]";
+    subtitles_textbox[2].innerHTML = "[silence]";
+    subtitles_textbox[3].innerHTML = "[silence]";
+    subtitles_textbox[4].innerHTML = "[silence]";
+    subtitles.innerHTML = "";
     return;
   }
 
@@ -37,11 +35,41 @@ function displaySubtitles_sync(){
   }
 
   var i = current_title_sync-1;
-  subtitles_textbox[0].value = (i-2>=0) ? subs[i-2]["text"] : "";
-  subtitles_textbox[1].value = (i-1>=0) ? subs[i-1]["text"] : "";
-  subtitles_textbox[2].value = (i>=0 && i < subs.length) ? subs[i]["text"] : "";
-  subtitles_textbox[3].value = (i+1<subs.length) ? subs[i+1]["text"] : "";
-  subtitles_textbox[4].value = (i+2<subs.length) ? subs[i+2]["text"] : "";
+  subtitles_textbox[0].innerHTML = (i-2>=0 && subs[i-2]["text"] != "") ? subs[i-2]["text"] : "[silence]";
+  subtitles_textbox[1].innerHTML = (i-1>=0 && subs[i-1]["text"] != "") ? subs[i-1]["text"] : "[silence]";
+  subtitles_p.innerHTML = subtitles_textbox[2].innerHTML = (i>=0 && i < subs.length && subs[i]["text"] != "") ? subs[i]["text"] : "[silence]";
+  if (subtitles_p.innerHTML == "[silence]") subtitles_p.innerHTML = ""
+  subtitles_textbox[3].innerHTML = (i+1<subs.length && subs[i+1]["text"] != "") ? subs[i+1]["text"] : "[silence]";
+  subtitles_textbox[4].innerHTML = (i+2<subs.length && subs[i+2]["text"] != "") ? subs[i+2]["text"] : "[silence]";
+}
+
+function sync_mode(){
+  var transcriptiondiv = document.getElementById("transcription");
+  var syncdiv = document.getElementById("syncing");
+  var titlesdiv = document.getElementById("titles_list");
+  var textinput = document.getElementById("textinput");
+
+  transcriptiondiv.style.display="none";
+  syncdiv.style.display="block";
+
+  textinput.style.display="none";
+  titlesdiv.style.display="block";
+
+  var titles = document.getElementById("titles_textarea").value.split('\n');
+  var subs = [];
+  for (i in titles){
+    subs.push({"text": titles[i], "start":-1,"end":-1});
+  }
+  current_subtitle = {"content":subs};
+  current_title_sync = 0;
+
+  //restart playback:
+  video.currentTime = 0;
+
+  video.addEventListener('timeupdate', function(e){
+    displaySubtitles_sync(e.target.currentTime*1000)
+  }, false);
+
 }
 
 function setup_autoskip(){
@@ -68,16 +96,74 @@ function autoskip_clicked(event){
 
 function load(event){
   video = document.getElementById("video");
-  subtitles_textbox.push(document.getElementById("textbox1"));
-  subtitles_textbox.push(document.getElementById("textbox2"));
-  subtitles_textbox.push(document.getElementById("textbox3"));
-  subtitles_textbox.push(document.getElementById("textbox4"));
-  subtitles_textbox.push(document.getElementById("textbox5"));
+  subtitles_p = document.getElementById("subtitles");
+
+  subtitles_textbox.push(document.getElementById("title_1"));
+  subtitles_textbox.push(document.getElementById("title_2"));
+  subtitles_textbox.push(document.getElementById("title_3"));
+  subtitles_textbox.push(document.getElementById("title_4"));
+  subtitles_textbox.push(document.getElementById("title_5"));
 
   autoskipback = document.getElementById("autoskipback");
   autoskipback_ammount = document.getElementById("autoskipback-ammount");
   autoskipback_interval = document.getElementById("autoskipback-interval");
 
   autoskipback.addEventListener("click", autoskip_clicked, false);
+
+
+  var select_key = function(value){
+    switch (value){
+      case "TAB": return 9;
+      case "CTRL": return 17;
+      case "[": return 219;
+      case "]": return 221;
+      case "SPACEBAR": return 32;
+      case "|": return 220;
+    }
+  };
+
+  function KeyUpHandler(event){
+    holdingkey = false;
+
+    var tap_key = select_key(document.getElementById("taphotkey").value);
+
+    if (event.which == tap_key){
+     if(current_title_sync == null) return;
+      var subs = current_subtitle["content"];
+      var now = Math.round(video.currentTime * 1000);
+      if (current_title_sync>0) subs[current_title_sync-1].end = now;
+      if (current_title_sync<subs.length){
+        subs[current_title_sync].start = now;
+        current_title_sync++;
+      }
+    }
+
+  }
+
+  function KeyDownHandler(event){
+    if (holdingkey==true) return;
+    holdingkey = true;
+
+    var rewind_key = select_key(document.getElementById("rewindhotkey").value);
+    var playpause_key = select_key(document.getElementById("playpausehotkey").value);
+
+    if (event.which == rewind_key){
+      var newval = video.currentTime - 3;
+      video.currentTime = (newval >= 0 ? newval : 0);
+      //TODO: inhibit key event propagation (?)
+    }
+
+    if (event.which == playpause_key){
+      if (video.paused || video.ended)
+          self.video.play();
+      else
+          video.pause();
+      //TODO: inhibit key event propagation (?)
+    }
+
+  };
+
+  window.addEventListener("keydown", KeyDownHandler, false);
+  window.addEventListener("keyup", KeyUpHandler, false);
   
 }
