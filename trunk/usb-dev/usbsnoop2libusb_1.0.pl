a7694c1f13c5157b433dc5546b6b26b68ed15adf
lucasvr <lucasvr@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-05-26 19:37:47 +0000

    Generating tabulated code to improve readability.

--- trunk/usb-dev/usbsnoop2libusb_1.0.pl
+++ trunk/usb-dev/usbsnoop2libusb_1.0.pl
@@ -53,7 +53,7 @@ sub process_urb {
     if ($text =~ m/coming back/) {
 		if ($show_received_bytes){
 			if ($text =~ m/:((\s[a-f0-9][a-f0-9])+)/) {
-				print "printf(\"received in the log: $1\\n\");\n";
+				print "\tprintf(\"received in the log: $1\\n\");\n";
 			}
 		}
 
@@ -78,8 +78,8 @@ sub process_urb {
 		if ($text =~ /URB (\d+) /) {
 		    my $urbnumber = $1;
 		    chomp($urbnumber);
-			print "ret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
-		    print "printf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
+			print "\tret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
+		    print "\tprintf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
 		}
 	    }
 	    $usbsnoopbugseen = 0;
@@ -103,7 +103,7 @@ sub process_urb {
 	    #$timediff = 2500; # FIXME
 	}
 #	if ($timediff > 0) {
-	    print "usleep($timediff*1000);\n";
+	    print "\tusleep($timediff*1000);\n";
 #	}
 	$last_time = $time;
     }
@@ -181,27 +181,27 @@ sub process_urb {
 	}
     }
     if ($text =~ m/URB_FUNCTION_GET_DESCRIPTOR_FROM_DEVICE/) {
-	print "ret = libusb_get_descriptor(devh, $DescriptorType, 0x$Index, buf, 0x$TransferBufferLength);\n";
-	print "printf(\"$urbnumber get descriptor returned %d, bytes: \\n \", ret);\n";
-	print "print_bytes(buf, ret);\n";
-	print "printf(\"\\n\");\n";
+	print "\tret = libusb_get_descriptor(devh, $DescriptorType, 0x$Index, buf, 0x$TransferBufferLength);\n";
+	print "\tprintf(\"$urbnumber get descriptor returned %d, bytes: \\n \", ret);\n";
+	print "\tprint_bytes(buf, ret);\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_GET_DESCRIPTOR_FROM_INTERFACE/) {
 	printf "ret = libusb_get_descriptor(devh, $DescriptorType, 0x$Index, buf, 0x$TransferBufferLength);\n";
-	print "printf(\"$urbnumber get descriptor returned %d, bytes: \\n \", ret);\n";
-	print "print_bytes(buf, ret);\n";
-	print "printf(\"\\n\");\n";
+	print "\tprintf(\"$urbnumber get descriptor returned %d, bytes: \\n \", ret);\n";
+	print "\tprint_bytes(buf, ret);\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_SELECT_CONFIGURATION/) {
 	if (defined($bConfigurationValue)) {
-	    print "ret = libusb_release_interface(devh, 0);\n";
-	    print "if (ret != 0) printf(\"failed to release interface before set_configuration: %d\\n\", ret);\n";
-	    print "ret = libusb_set_configuration(devh, 0x$bConfigurationValue);\n";
-	    print "printf(\"$urbnumber set configuration returned %d\\n\", ret);\n";
-	    print "ret = libusb_claim_interface(devh, 0);\n";
-	    print "if (ret != 0) printf(\"claim after set_configuration failed with error %d\\n\", ret);\n";
+	    print "\tret = libusb_release_interface(devh, 0);\n";
+	    print "\tif (ret != 0) printf(\"failed to release interface before set_configuration: %d\\n\", ret);\n";
+	    print "\tret = libusb_set_configuration(devh, 0x$bConfigurationValue);\n";
+	    print "\tprintf(\"$urbnumber set configuration returned %d\\n\", ret);\n";
+	    print "\tret = libusb_claim_interface(devh, 0);\n";
+	    print "\tif (ret != 0) printf(\"claim after set_configuration failed with error %d\\n\", ret);\n";
 	}
 	if (defined($AlternateSetting)) {
-		print "ret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
-	    print "printf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
+		print "\tret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
+	    print "\tprintf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
 	}    
     } elsif ($text =~ m/URB_FUNCTION_CLASS_INTERFACE/) {
 	my $requesttype = "LIBUSB_REQUEST_TYPE_CLASS + LIBUSB_RECIPIENT_INTERFACE";
@@ -210,16 +210,16 @@ sub process_urb {
 	}
 	my $bytes = get_transferbuffer_contents(@lines);
 	if ($bytes ne "") {
-	    print "memcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
+	    print "\tmemcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
 	}
-	print "ret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
+	print "\tret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
 
 	if ($text =~ m/USBD_TRANSFER_DIRECTION_IN/) {
-	    print "printf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\nprint_bytes(buf, ret);\n";
+	    print "\tprintf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\nprint_bytes(buf, ret);\n";
 	} else {
-	    print "printf(\"$urbnumber control msg returned %d\", ret);\n";
+	    print "\tprintf(\"$urbnumber control msg returned %d\", ret);\n";
 	}
-	print "printf(\"\\n\");\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_VENDOR_INTERFACE/) {
 	my $requesttype = "LIBUSB_REQUEST_TYPE_VENDOR + LIBUSB_RECIPIENT_INTERFACE";
 	if ($text =~ m/USBD_TRANSFER_DIRECTION_IN/) {
@@ -227,12 +227,12 @@ sub process_urb {
 	}
 	my $bytes = get_transferbuffer_contents(@lines);
 	if ($bytes ne "") {
-	    print "memcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
+	    print "\tmemcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
 	}
-	print "ret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
-	print "printf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\n";
-	print "print_bytes(buf, ret);\n";
-	print "printf(\"\\n\");\n";
+	print "\tret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
+	print "\tprintf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\n";
+	print "\tprint_bytes(buf, ret);\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_BULK_OR_INTERRUPT_TRANSFER/) {
 	my $method;
 	if ( ( hex $endpoint & 128 ) == 128 ) {
@@ -255,24 +255,24 @@ sub process_urb {
 	}
 	my $bytes = get_transferbuffer_contents(@lines);
 	if ($bytes ne "" and $method eq "write") {
-	    print "memcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
+	    print "\tmemcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
 	}
 	my $timeout = 1000;
 	if ($mode eq "bulk" and hex($TransferBufferLength) > 50) {
 	    $timeout = 1000 + hex($TransferBufferLength) * 0.06;
 	    $timeout = sprintf("%.0d", $timeout); # round to integer
 	}
-	print "ret = usb_".$mode."_$method(devh, 0x$endpoint, buf, 0x$TransferBufferLength, $timeout);\n";
-	print "printf(\"$urbnumber $mode $method returned %d, bytes: \\n \", ret);\n";
+	print "\tret = usb_".$mode."_$method(devh, 0x$endpoint, buf, 0x$TransferBufferLength, $timeout);\n";
+	print "\tprintf(\"$urbnumber $mode $method returned %d, bytes: \\n \", ret);\n";
 # sounds weird but write requests can actually also return data
 #	if ($method eq "read") {
-	    print "print_bytes(buf, ret);\n";
+	    print "\tprint_bytes(buf, ret);\n";
 #	}
-	print "printf(\"\\n\");\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_SET_FEATURE_TO_DEVICE/) {
 	my $requesttype = "LIBUSB_REQUEST_TYPE_STANDARD + LIBUSB_RECIPIENT_DEVICE";
-	print "ret = libusb_control_transfer(devh, $requesttype, USB_REQ_SET_FEATURE, $FeatureSelector, 0, buf, 0, 1000);\n";
-	print "printf(\"$urbnumber set feature request returned %d\\n\", ret);\n";
+	print "\tret = libusb_control_transfer(devh, $requesttype, USB_REQ_SET_FEATURE, $FeatureSelector, 0, buf, 0, 1000);\n";
+	print "\tprintf(\"$urbnumber set feature request returned %d\\n\", ret);\n";
     } elsif ($text =~ m/URB_FUNCTION_VENDOR_DEVICE/) {
 	my $requesttype = "LIBUSB_REQUEST_TYPE_VENDOR + LIBUSB_RECIPIENT_DEVICE";
 	if ($text =~ m/USBD_TRANSFER_DIRECTION_IN/) {
@@ -280,12 +280,12 @@ sub process_urb {
 	}
 	my $bytes = get_transferbuffer_contents(@lines);
 	if ($bytes ne "") {
-	    print "memcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
+	    print "\tmemcpy(buf, \"$bytes\", 0x$TransferBufferLength);\n";
 	}
-	print "ret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
-	print "printf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\n";
-	print "print_bytes(buf, ret);\n";
-	print "printf(\"\\n\");\n";
+	print "\tret = libusb_control_transfer(devh, $requesttype, 0x$Request, 0x$Value, 0x$Index, buf, 0x$TransferBufferLength, 1000);\n";
+	print "\tprintf(\"$urbnumber control msg returned %d, bytes: \\n \", ret);\n";
+	print "\tprint_bytes(buf, ret);\n";
+	print "\tprintf(\"\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_GET_CURRENT_FRAME_NUMBER/) {
 	my $requesttype = "LIBUSB_REQUEST_TYPE_VENDOR + LIBUSB_RECIPIENT_DEVICE";
 	# TODO
@@ -301,26 +301,26 @@ sub process_urb {
 	if ($packetcount * $packetsize > $maxIsoBufLength) {
 	    die "packetcount ($packetcount) or packetsize ($packetsize) is way too large!\nMaybe you could increase the \$maxIsoBufLength parameter in usbsnoop2libusb_1.0.pl\n";
 	}
-	print "transfer = libusb_alloc_transfer($packetcount);\n";
-	print "libusb_fill_iso_transfer(transfer, devh, 0x$endpoint, isobuf, $packetsize * $packetcount, $packetcount, iso_callback, NULL, 10000);\n";
-	print "ret = libusb_submit_transfer(transfer);\n";
-	print "if (ret) printf(\"$urbnumber isochronous submit returned %d\\n\", ret);\n";
+	print "\ttransfer = libusb_alloc_transfer($packetcount);\n";
+	print "\tlibusb_fill_iso_transfer(transfer, devh, 0x$endpoint, isobuf, $packetsize * $packetcount, $packetcount, iso_callback, NULL, 10000);\n";
+	print "\tret = libusb_submit_transfer(transfer);\n";
+	print "\tif (ret) printf(\"$urbnumber isochronous submit returned %d\\n\", ret);\n";
 	# TODO
     } elsif ($text =~ m/URB_FUNCTION_CLEAR_FEATURE_TO_ENDPOINT/) {
 		my $requesttype = "LIBUSB_REQUEST_TYPE_STANDARD + LIBUSB_RECIPIENT_ENDPOINT";
-		print "ret = libusb_control_transfer(devh, $requesttype, LIBUSB_REQUEST_CLEAR_FEATURE, $FeatureSelector, 0, buf, 0, 1000);\n";
-		print "printf(\"$urbnumber clear feature request returned %d\\n\", ret);\n";
+		print "\tret = libusb_control_transfer(devh, $requesttype, LIBUSB_REQUEST_CLEAR_FEATURE, $FeatureSelector, 0, buf, 0, 1000);\n";
+		print "\tprintf(\"$urbnumber clear feature request returned %d\\n\", ret);\n";
     } elsif ($text =~ m/URB_FUNCTION_ABORT_PIPE/) {
-		print "libusb_cancel_transfer(transfer);\n";
+		print "\tlibusb_cancel_transfer(transfer);\n";
     } elsif ($text =~ m/URB_FUNCTION_RESET_PIPE/) {
 	# TODO: implement
-	print "printf(\"$urbnumber reset pipe not implemented yet :(\\n\");\n";
+	print "\tprintf(\"$urbnumber reset pipe not implemented yet :(\\n\");\n";
     } elsif ($text =~ m/URB_FUNCTION_SELECT_INTERFACE/) {
         if (!defined($AlternateSetting)) {
             die "can't find alternatesetting\n";
         }
-		print "ret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
-        print "printf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
+		print "\tret = libusb_set_interface_alt_setting(devh,	0, $AlternateSetting);\n";
+        print "\tprintf(\"$urbnumber set alternate setting returned %d\\n\", ret);\n";
     } elsif ($text =~ m/incorrect UrbHeader.Length=0,/) {
 	# ignore
     } elsif ($text =~ m/non printable URB with function code 0x0000002a/) {
@@ -339,17 +339,17 @@ print <<EOF
 #include <libusb-1.0/libusb.h>
 
 void print_bytes(char *bytes, int len) {
-    int i;
-    if (len > 0) {
+	int i;
+	if (len > 0) {
 	for (i=0; i<len; i++) {
-	    printf("%02x ", (int)((unsigned char)bytes[i]));
+		printf("%02x ", (int)((unsigned char)bytes[i]));
 	}
 	printf("\\"");
-        for (i=0; i<len; i++) {
-	    printf("%c", isprint(bytes[i]) ? bytes[i] : '.');
-        }
-        printf("\\"");
-    }
+		for (i=0; i<len; i++) {
+		printf("%c", isprint(bytes[i]) ? bytes[i] : '.');
+		}
+		printf("\\"");
+	}
 }
 
 static void iso_callback(struct libusb_transfer *transfer){
@@ -368,35 +368,35 @@ static void iso_callback(struct libusb_transfer *transfer){
 }
 
 int main(int argc, char **argv) {
-    int ret, vendor, product;
+	int ret, vendor, product;
 	char buf[65535];
 	char isobuf[$maxIsoBufLength];
 	static struct libusb_device_handle *devh = NULL;
 	struct libusb_transfer* transfer;
 
-    if (argc!=3) {
-	printf("usage: %s vendorID productID\\n", argv[0]);
-	exit(1);
-    }
+	if (argc!=3) {
+		printf("usage: %s vendorID productID\\n", argv[0]);
+		exit(1);
+	}
 
-    char *endptr;
-    vendor = strtol(argv[1], &endptr, 16);
-    if (*endptr != '\\0') {
-	printf("invalid vendor id\\n");
-	exit(1);
-    }
-    product = strtol(argv[2], &endptr, 16);
-    if (*endptr != '\\0') {
-	printf("invalid product id\\n");
-	exit(1);
-    }
+	char *endptr;
+	vendor = strtol(argv[1], &endptr, 16);
+	if (*endptr != '\\0') {
+		printf("invalid vendor id\\n");
+		exit(1);
+	}
+	product = strtol(argv[2], &endptr, 16);
+	if (*endptr != '\\0') {
+		printf("invalid product id\\n");
+		exit(1);
+	}
 
 
 	ret = libusb_init(NULL);
 	if (ret < 0)
 		return ret;
-	
-    libusb_set_debug(NULL, 3);
+
+	libusb_set_debug(NULL, 3);
 	devh = libusb_open_device_with_vid_pid(NULL, vendor, product);
 
 EOF
