549734a81ff9bc4fe5c1841e2a1523f4ad52a25c
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-11-25 03:14:27 +0000

    * wrapped it into a class and instantiated several display modules. * implemented simple graphics rendering

--- SVG/SubwayDisplay/SubwayDisplay.js
+++ SVG/SubwayDisplay/SubwayDisplay.js
@@ -1,43 +1,145 @@
-var svgDocument;
-var sprites;
-var NUM_SPRITES;
-
-var delta_t = 50; //frequency = 1/50ms = 20Hz
+var modules = new Array();
 
 function init(evt){
-	sprites = new Array();
-
 	var O=evt.target;
-	svgDocument=O.ownerDocument;
+	var svgDoc=O.ownerDocument;
 
-  for (var i=0;i<10;i++)
-  	sprites.push(svgDocument.getElementById("seg0"+i));
-  for (var i=10;i<=64;i++)
-  	sprites.push(svgDocument.getElementById("seg"+i));
-
-	NUM_SPRITES=64;
-	window.setInterval(mytimer, delta_t);
-}
-
-function mytimer(){
-		random_flashing();
-}
-
-function turn_on(i){
-	if (sprites[i]){
-		sprites[i].setAttribute("style", "fill:#fcfb79");
-	}
-}
-
-function turn_off(i){
-	if (sprites[i]){
-		sprites[i].setAttribute("style", "fill:#4d1908");
+  for (var i=0;i<5;i++){
+    modules.push(new Display(svgDoc, i, 0));
   }
+  for (var i=0;i<5;i++){
+    modules.push(new Display(svgDoc, i, 1));
+  }
+
+  var delta_t = 100;
+   //frequency = 1/200ms = 5Hz
+	window.setInterval(timer, delta_t);
 }
 
-function random_flashing(){
-	for (i=0;i<20;i++){
-		turn_on(Math.floor(Math.random()*NUM_SPRITES));
-		turn_off(Math.floor(Math.random()*NUM_SPRITES));
+var xx=0;
+function drawing_function(p){
+  var middle = p[p.length-1];
+  var x=middle[0];
+  var y=middle[1];
+
+  return (x+y<xx);
+}
+
+function timer(){
+  for (m in modules){
+    var mod = modules[m];
+    mod.draw(drawing_function);
+  }
+  xx+=20;
+  if (xx>1500) xx=0;
+}
+
+var Display = function(svg, col, row){
+  this.shapes = new Array();
+
+//todo. calc these values instead of using hardcoded width&height
+  this.x0 = col*172;
+  this.y0 = row*218;
+
+  var theDisplay = svg.getElementById("display").cloneNode(true);
+  
+  svg.getElementsByTagName("svg")[0].appendChild(theDisplay);
+  theDisplay.setAttribute("transform", "translate("+this.x0+","+this.y0+")");
+  
+  var group = theDisplay.getElementsByTagName("g")[0];
+  for (var n in group.childNodes){
+    var node = group.childNodes[n];
+    if (node.tagName == "path") this.shapes.push(node);
+  }
+
+/*
+  var delta_t = 50; //frequency = 1/200ms = 5Hz
+  var self = this;
+	window.setInterval(function(){self.timer()}, delta_t);
+*/
+}
+
+Display.prototype.timer = function(){
+  this.random_flashing();
+}
+
+Display.prototype.turn_on = function(i){
+	if (this.shapes[i])	this.turn_on_shape(this.shapes[i]);
+}
+
+Display.prototype.turn_off = function(i){
+	if (this.shapes[i])	this.turn_off_shape(this.shapes[i]);
+}
+
+Display.prototype.turn_on_shape = function(s){
+	if (s) s.setAttribute("style", "fill:#fcfb79");
+}
+
+Display.prototype.turn_off_shape = function(s){
+	if (s) s.setAttribute("style", "fill:#4d1908");
+}
+
+Display.prototype.random_flashing = function(){
+	for (var i=0;i<20;i++){
+		this.turn_on(Math.floor(Math.random()*this.shapes.length));
+		this.turn_off(Math.floor(Math.random()*this.shapes.length));
 	}
 }
+
+Display.prototype.draw = function(func){
+	for (var s in this.shapes){
+	  var shape = this.shapes[s];
+	  if (!shape.points){
+	    shape.points = new Array();
+
+      var x=0,y=0;
+      var mx=0,my=0;
+      var scale = 0.2038;
+	    for (var i = 0; i < shape.pathSegList.numberOfItems; i++){
+        var seg = shape.pathSegList.getItem(i);
+
+        switch(seg.pathSegType){
+          case seg.PATHSEG_MOVETO_REL:
+            x += seg.x;
+            y += seg.y;
+            break;
+          case seg.PATHSEG_MOVETO_ABS:
+            x = seg.x;
+            y = seg.y;
+            break;
+          case seg.PATHSEG_LINETO_REL:
+            mx+=x;
+            my+=y;
+            shape.points.push([this.x0 + x*scale,this.y0 + y*scale]);
+            x += seg.x;
+            y += seg.y;
+            break;
+          case seg.PATHSEG_LINETO_ABS:
+            mx+=x;
+            my+=y;
+            shape.points.push([this.x0 + x*scale,this.y0 + y*scale]);
+            x = seg.x;
+            y = seg.y;
+            break;
+          case seg.PATHSEG_CLOSE:
+            mx+=x;
+            my+=y;
+            shape.points.push([this.x0 + x*scale,this.y0 + y*scale]);
+          default:
+        }
+      }
+
+      shape.points.push([this.x0 + mx*scale/shape.points.length,this.y0 + my*scale/shape.points.length]);
+    }
+
+    if (func(shape.points))
+      this.turn_on_shape(shape);
+    else
+      this.turn_off_shape(shape);
+
+	}
+
+
+}
+
+
