69dfdbca92efa040593f84d5989cf10296446f5b
czestmyr <czestmyr@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-12-09 06:20:45 +0000

    Pong game: implemented game physics and started work on numbers

--- trunk/laserdisplay/example2.py
+++ trunk/laserdisplay/example2.py
@@ -12,6 +12,17 @@ GREEN = [0x00,0xFF,0x00] #green
 BLUE = [0x00,0x00,0xFF] #blue
 
 CIRCLE = [[0.0,0.5],[0.0,1.0],[0.5,1.0],[1.0,1.0],[1.0,0.5],[1.0,0.0],[0.5,0.0],[0.0,0.0],[0.0,0.5]]
+NUMBERS = [
+  [[0.0,0.5],[0.0,1.0],[0.5,1.0],[1.0,1.0],[1.0,0.5],[1.0,0.0],[0.5,0.0],[0.0,0.0],[0.0,0.5]],
+  [[0.0,0.0],[0.0,0.5],[0.0,1.0]],
+  [[0.0,0.5],[0.0,1.0],[0.9,1.0],[1.8,0.5],[0.0,0.0],[0.5,0.0],[1.0,0.0]]
+]
+
+def gen_number(num, x, y, rx, ry):
+  number = []
+  for i in range(len(NUMBERS[num])):
+    number.append([(int)(x + (NUMBERS[num][i][0]-0.5)*rx),(int)(y + (NUMBERS[num][i][1]-0.5)*ry)]);
+  return number
 
 def gen_circle(x, y, r):
   points = []
@@ -25,14 +36,18 @@ circle1 = gen_circle(0x80, 0x80, 0x10);
 circle2 = gen_circle(0x80, 0x80, 0x18);
 circle3 = gen_circle(0x80, 0x80, 0x20);
 
-time = 0
-
-FPS = 30
-
 pygame.init()
 
 size=width,height=320,200;screen=pygame.display.set_mode(size);
 clock = pygame.time.Clock()
+
+FPS = 30
+PLAYER1_X = 20
+PLAYER2_X = 230
+BARRIER1 = 15
+BARRIER2 = 235
+BARRIER_TOP = 235
+BARRIER_BOTTOM = 15
 
 player1 = 128
 player2 = 128
@@ -40,33 +55,56 @@ dir1 = 0
 dir2 = 0
 ballx = 128
 bally = 128
+ball_dx = 2
+ball_dy = 0
+acc1 = 0
+acc2 = 0
 
-while True:
-  clock.tick(FPS)
-  time += 1
+time = 0
+shutdown = 0
 
+DIR_TBL = [3, 2, 1, 0, -1, -2, -3]
+bounce_idx = 3
+
+def reset():
+  global player1, player2, dir1, dir2, ballx, bally, ball_dx, ball_dy, acc1, acc2
+  player1 = 128
+  player2 = 128
+  dir1 = 0
+  dir2 = 0
+  ballx = 128
+  bally = 128
+  ball_dx = 2
+  ball_dy = 0
+  bounce_idx = 3
   acc1 = 0
   acc2 = 0
+
+while shutdown != 1:
+  clock.tick(FPS)
+  time += 1
 
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_q:
-        acc2 += 3
+        acc2 += 2
       if event.key == pygame.K_a:
-        acc2 -= 3
+        acc2 -= 2
       if event.key == pygame.K_o:
-        acc1 += 3
+        acc1 += 2
       if event.key == pygame.K_l:
-        acc1 -= 3
+        acc1 -= 2
+      if event.key == pygame.K_ESCAPE:
+        shutdown = 1
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_q:
-        acc2 -= 3
+        acc2 -= 2
       if event.key == pygame.K_a:
-        acc2 += 3
+        acc2 += 2
       if event.key == pygame.K_o:
-        acc1 -= 3
+        acc1 -= 2
       if event.key == pygame.K_l:
-        acc1 += 3
+        acc1 += 2
 
   dir1 += acc1
   dir2 += acc2
@@ -83,18 +121,69 @@ while True:
   if player2 > 230:
     player2 = 230
 
-  x_coord = (int)(math.cos(time/200.0) * 40) + 128
-  y_coord = (int)(math.sin(time/200.0) * 40) + 128
+  ball_dy = DIR_TBL[bounce_idx]
 
-  circle = gen_circle(x_coord, y_coord, 5)
-  circle2 = gen_circle(x_coord, y_coord, 3)
+  ballx += ball_dx
+  bally += ball_dy
+
+  region = -1
+
+  if ballx < PLAYER1_X:
+    diff = bally - player1
+    if diff > 20 or diff < -20:
+      reset()
+    elif diff >= 15:
+      region = 0
+    elif diff >= 10:
+      region = 1
+    elif diff > -10:
+      region = 2
+    elif diff > -15:
+      region = 3
+    else:
+      region = 4
+  if ballx > PLAYER2_X:
+    diff = bally - player2
+    if diff > 20 or diff < -20:
+      reset()
+    elif diff >= 15:
+      region = 0
+    elif diff >= 10:
+      region = 1
+    elif diff > -10:
+      region = 2
+    elif diff > -15:
+      region = 3
+    else:
+      region = 4
+
+  if region != -1:
+    bounce_idx = region + bounce_idx - 2
+    if bounce_idx < 0: bounce_idx = 0
+    if bounce_idx > 6: bounce_idx = 6
+    ball_dx = -ball_dx
+
+  if bally < BARRIER_BOTTOM or bally > BARRIER_TOP:
+    bounce_idx = 6 - bounce_idx
+
+  if (ballx < BARRIER1):
+    reset()
+  if (ballx > BARRIER2):
+    reset()
+
+  circle = gen_circle(ballx, bally, 5)
+  circle2 = gen_circle(ballx, bally, 3)
 
   LD.set_color(RED)
   LD.draw_bezier(circle, 10)
   LD.set_color(GREEN)
   LD.draw_bezier(circle2, 6)
   LD.set_color([0xff,0x00,0x20])
-  LD.draw_line(20, player1-20, 20, player1+20)
+  LD.draw_line(PLAYER1_X, player1-20, PLAYER1_X, player1+20)
   LD.set_color([0x20,0xff,0xff])
-  LD.draw_line(230, player2-20, 230, player2+20)
+  LD.draw_line(PLAYER2_X, player2-20, PLAYER2_X, player2+20)
+
+  number1 = gen_number(2, 128, 245, 8, 20)
+  LD.set_color(RED)
+  LD.draw_bezier(number1, 4)
 
