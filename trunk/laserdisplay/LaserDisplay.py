54ac24e5ecbc281403466b1fb76035fd77be6a98
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-12-10 13:52:27 +0000

    add colors to clock. add color names to main module

--- trunk/laserdisplay/LaserDisplay.py
+++ trunk/laserdisplay/LaserDisplay.py
@@ -3,8 +3,16 @@
 import usb.core
 import usb.util
 import math
-PI=3.1415
 from random import random
+PI=3.1415
+
+RED = [255,0,0]
+GREEN = [0,255,0]
+BLUE = [0,0,255]
+CYAN = [0,255,255]
+MAGENTA = [255,0,255]
+YELLOW = [255,255,0]
+WHITE = [255,255,255]
 
 def clamp(value, min, max):
   if value > max: return max
