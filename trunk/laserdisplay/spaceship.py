f7916cdf111cd468c23b5c40431e37ec2c31bf8b
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-12-10 17:33:10 +0000

    Added matrix transforms to the framework. we'll work on a spaceship game!

---
+++ trunk/laserdisplay/spaceship.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from LaserDisplay import *
+import math
+import random
+
+WIDTH = 100
+HEIGHT = 100
+
+LD = LaserDisplay()
+
+ship = [[190, 64], [168, 136], [118, 200], [77, 142], [63, 63], [87, 77], [105, 103], [119, 114], [143, 104], [158, 81], [192, 63]]
+
+cx,cy = WIDTH/2, HEIGHT/2
+
+angle=0
+while True:
+    angle+=0.1
+    LD.set_color(RED)
+    LD.save()
+    LD.rotate_at(cx,cy,angle)
+    LD.draw_bezier(ship, 10)
+    LD.restore()
+
+
