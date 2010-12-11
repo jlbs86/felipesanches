31bd2affd4f7358fedcb42ec9a4e90c6c00b135b
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-12-11 06:06:12 +0000

    add quadratic & cubic bezier commands to server

--- trunk/laserdisplay/laser-server.py
+++ trunk/laserdisplay/laser-server.py
@@ -5,12 +5,21 @@ from twisted.internet.protocol import Factory, Protocol
 from twisted.internet.task import LoopingCall 
 from twisted.internet import reactor
 
+QUAD_BEZ_QUALITY = 8
+CUBIC_BEZ_QUALITY = 8
+
 def update_laser (connections):
   LD.start_frame()
   for con in connections:
     for cmd in con.buffered_commands:
       if cmd[0] == "line":
         msg = LD.line_message(int(cmd[1]), int(cmd[2]), int(cmd[3]), int(cmd[4]))
+        LD.schedule(msg)
+      if cmd[0] == "quadratic":
+        msg = LD.quadratic_bezier_message([[int(cmd[1]), int(cmd[2])], [int(cmd[3]), int(cmd[4])], [int(cmd[5]), int(cmd[6])]], QUAD_BEZ_QUALITY)
+        LD.schedule(msg)
+      if cmd[0] == "cubic":
+        msg = LD.cubic_bezier_message([[int(cmd[1]), int(cmd[2])], [int(cmd[3]), int(cmd[4])], [int(cmd[5]), int(cmd[6])], [int(cmd[7]), int(cmd[8])]], CUBIC_BEZ_QUALITY)
         LD.schedule(msg)
       if cmd[0] == "color":
         LD.set_color([int(cmd[1]), int(cmd[2]), int(cmd[3])])
@@ -21,7 +30,7 @@ loop = LoopingCall(update_laser, connections)
 loop.start(0)
 
 class SendContent(Protocol):
-    valid_commands = ["line", "color", "show", "quit"]
+    valid_commands = ["quadratic", "cubic", "line", "color", "show", "quit"]
 
     def __init__(self):
       connections.append(self)
