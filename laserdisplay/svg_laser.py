647d78189878327e7a4b37861b8486d0b9033c68
felipe.sanches <felipe.sanches@be285416-c6b8-11dd-9042-6d746b85d38b>
2010-12-15 16:10:01 +0000

    updating svg laser to use more recent API

--- laserdisplay/svg_laser.py
+++ laserdisplay/svg_laser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from LaserDisplay import LaserDisplay
+from LaserDisplay import *
 
 LD = LaserDisplay({"server":"localhost","port": 50000})
 #LD = LaserDisplay()
@@ -10,7 +10,6 @@ filename = sys.argv[1]
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler 
 
-messages = []
 class SVGHandler(ContentHandler):
   def startElement(self, name, attrs):
     if name=="path":
@@ -19,7 +18,6 @@ class SVGHandler(ContentHandler):
       x,y=0,0
       x0,y0=x,y
       
-      LD.start_frame()
       while i<len(tokens):
         if tokens[i].lower() in ["m", "c", "l", "h", "v", "z"]:
           cmd = tokens[i]
@@ -39,39 +37,39 @@ class SVGHandler(ContentHandler):
           cmd="l"
         elif cmd=="l":
           delta=tokens[i].split(",")
-          LD.schedule(LD.line_message(x,y,x+float(delta[0]),y+float(delta[1])))
+          LD.draw_line(x,y,x+float(delta[0]),y+float(delta[1]))
           x+=float(delta[0])
           y+=float(delta[1])
         elif cmd=="L":
           delta=tokens[i].split(",")
-          LD.schedule(LD.line_message(x,y,float(delta[0]),float(delta[1])))
+          LD.draw_line(x,y,float(delta[0]),float(delta[1]))
           x=float(delta[0])
           y=float(delta[1])
         elif cmd=="z" or cmd=="Z":
           i-=1
-          LD.schedule(LD.line_message(x,y,x0,y0))
+          LD.draw_line(x,y,x0,y0)
           x,y=x0,y0
         elif cmd=="h":
           delta=tokens[i]
-          LD.schedule(LD.line_message(x,y,x+float(delta),y))
+          LD.draw_line(x,y,x+float(delta),y)
           x+=delta
         elif cmd=="H":
           delta=tokens[i]
-          LD.schedule(LD.line_message(x,y,float(delta),y))
+          LD.draw_line(x,y,float(delta),y)
           x=delta
         elif cmd=="v":
           delta=tokens[i]
-          LD.schedule(LD.line_message(x,y,x,y+float(delta)))
+          LD.draw_line(x,y,x,y+float(delta))
           y+=delta
         elif cmd=="V":
           delta=tokens[i]
-          LD.schedule(LD.line_message(x,y,x,float(delta)))
+          LD.draw_line(x,y,x,float(delta))
           y=delta
         elif cmd=="c":
           ctrl1=tokens[i].split(",")
           ctrl2=tokens[i+1].split(",")
           delta=tokens[i+2].split(",")
-          LD.schedule(LD.cubic_bezier_message([[x,y],[x+float(ctrl1[0]),y+float(ctrl1[1])],[x+float(ctrl2[0]),y+float(ctrl2[1])],[x+float(delta[0]),y+float(delta[1])]],15))
+          LD.draw_cubic_bezier([[x,y],[x+float(ctrl1[0]),y+float(ctrl1[1])],[x+float(ctrl2[0]),y+float(ctrl2[1])],[x+float(delta[0]),y+float(delta[1])]],15)
           x+=float(delta[0])
           y+=float(delta[1])
           i+=2
@@ -79,23 +77,21 @@ class SVGHandler(ContentHandler):
           ctrl1=tokens[i].split(",")
           ctrl2=tokens[i+1].split(",")
           delta=tokens[i+2].split(",")
-          LD.schedule(LD.cubic_bezier_message([[x,y],[float(ctrl1[0]),float(ctrl1[1])],[float(ctrl2[0]),float(ctrl2[1])],[float(delta[0]),float(delta[1])]],15))
+          LD.draw_cubic_bezier([[x,y],[float(ctrl1[0]),float(ctrl1[1])],[float(ctrl2[0]),float(ctrl2[1])],[float(delta[0]),float(delta[1])]],15)
           x+=float(delta[0])
           y+=float(delta[1])
           i+=2
         i+=1
-      messages.append(LD.messageBuffer)
 
 #we want to parse this: <path d=""/>
 parser = make_parser()   
 parser.setContentHandler(SVGHandler())
 parser.parse(open(filename))
 
-LD.start_frame()
-for m in messages:
-  LD.schedule(m)
 mess = LD.messageBuffer
+LD.set_scan_rate(35000)
+LD.set_blanking_delay(0)
 
 while True:
     LD.messageBuffer = mess
-    LD.end_frame()
+    LD.show_frame()
